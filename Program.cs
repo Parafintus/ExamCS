782ba59b5bb4582432cee83798202803ad97ac6b
Nikita <ba999@mail.ru>
2023-03-23 22:30:23 +0300

    Create method

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string[] array1 = new string[5] { "987", "Geek", "hello", "world", "res" };
+string[] array2 = new string[array1.Length];
+void FindElementsToArray(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if (arr1[i].Length <= 3)
+        {
+            arr2[count] = arr1[i];
+            count++;
+        }
+    }
+}
